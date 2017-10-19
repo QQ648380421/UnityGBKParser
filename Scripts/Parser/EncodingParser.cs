d4e83e3904c9e1ba0444c4a48bb775b195414fd3
Rect <rectvv@gmail.com>
2017-10-19 17:46:03 +0800

    add source code

---
+++ Scripts/Parser/EncodingParser.cs
@@ -0,0 +1,111 @@
+﻿//*************************************************************************
+//	创建日期:	2017-10-11
+//	文件名称:	EncodingParser.cs
+//  创建作者:	Rect 	
+//	版权所有:	shadowkong.com
+//	相关说明:	
+//*************************************************************************
+
+//-------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace sk
+{
+    class EncodingParser
+    {
+        #region Member variables
+        //-------------------------------------------------------------------------
+        protected Encoding m_GB2312;
+        protected Encoding m_UTF8;
+        protected Encoding m_ASCII;
+        //-------------------------------------------------------------------------
+        #endregion
+
+        #region Public Method
+        //-------------------------------------------------------------------------
+        public EncodingParser()
+        {
+            if (RuntimePlatform.IPhonePlayer == Application.platform)
+            {
+                // Mono编译模式下
+                // m_GB2312 = Encoding.GetEncoding(936);
+
+                // iL2CPP编译模式下
+                m_GB2312 = new iOSGBKEncoding();
+
+            }
+            else
+            {
+                m_GB2312 = Encoding.GetEncoding("gb2312");
+
+            }
+            m_UTF8 = Encoding.UTF8;
+            m_ASCII = Encoding.ASCII;
+        }
+        //-------------------------------------------------------------------------
+        public string GB2312ToUtf8(string gb2312String)
+        {
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                Debug.Log("_GB2312ToUtf8 转换前 = " + gb2312String);
+                string s = NativeiOS.MFWGB2312toUTF8(gb2312String, gb2312String.Split(NativeiOS.m_bSplit).Length);
+                Debug.Log("_GB2312ToUtf8 转换后 = " + s);
+                
+                return s;
+            }
+            else
+            {
+                return _EncodingConvert(gb2312String, m_GB2312, m_UTF8);
+            }
+        }
+        //-------------------------------------------------------------------------
+        public string Utf8ToGB2312(string utf8String)
+        {
+
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                Debug.Log("_Utf8ToGB2312 转换前 = " + utf8String);
+                string s = NativeiOS.MFWUTF8ToGB2312(utf8String);
+                Debug.Log("_Utf8ToGB2312 转换后 = " + s);
+                return s;
+            }
+            else
+            {
+                return _EncodingConvert(utf8String, m_UTF8, m_GB2312);
+            }
+        }
+        //-------------------------------------------------------------------------
+        public string ASCIIToUtf8(string asciiString)
+        {
+            return _EncodingConvert(asciiString, m_ASCII, m_UTF8);
+        }
+        //-------------------------------------------------------------------------
+        public string Utf8ToASCII(string utf8String)
+        {
+            return _EncodingConvert(utf8String, m_UTF8, m_ASCII);
+        }
+        //-------------------------------------------------------------------------
+        #endregion
+
+        #region private Method
+        //-------------------------------------------------------------------------
+        private string _EncodingConvert(string fromString, Encoding fromEncoding, Encoding toEncoding)
+        {
+
+            byte[] fromBytes = fromEncoding.GetBytes(fromString);
+            byte[] toBytes = Encoding.Convert(fromEncoding, toEncoding, fromBytes);
+
+            string toString = toEncoding.GetString(toBytes);
+            return toString;
+
+
+        }
+        //-------------------------------------------------------------------------
+        #endregion
+
+    }
+}
---
+++ Scripts/Parser/iOSGBKEncoding.cs
@@ -0,0 +1,85 @@
+﻿//*************************************************************************
+//	创建日期:	2017-10-11
+//	文件名称:	iOSGBKEncoding.cs
+//  创建作者:	Rect 	
+//	版权所有:	shadowkong.com
+//	相关说明:	
+//*************************************************************************
+
+//-------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace sk
+{
+    class iOSGBKEncoding : Encoding
+    {
+        //-------------------------------------------------------------------------
+        //-------------------------------------------------------------------------
+        public override int GetByteCount(char[] chars, int index, int count)
+        {
+            return UTF8.GetByteCount(chars, index, count);
+        }
+        //-------------------------------------------------------------------------
+        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
+        {
+            return UTF8.GetBytes(chars, charIndex, charCount, bytes, byteIndex);
+        }
+        //-------------------------------------------------------------------------
+        public override int GetCharCount(byte[] bytes, int index, int count)
+        {
+            return UTF8.GetCharCount(bytes, index, count);
+        }
+        //-------------------------------------------------------------------------
+        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
+        {
+            return UTF8.GetChars(bytes, byteIndex, byteCount, chars, charIndex);
+        }
+        //-------------------------------------------------------------------------
+        public override int GetMaxByteCount(int charCount)
+        {
+            return UTF8.GetMaxByteCount(charCount);
+        }
+        //-------------------------------------------------------------------------
+        public override int GetMaxCharCount(int byteCount)
+        {
+            return UTF8.GetMaxCharCount(byteCount);
+        }
+        //-------------------------------------------------------------------------
+        public override string GetString(byte[] bytes, int index, int count)
+        {
+            if (null == bytes || count > bytes.Length)
+            {
+                return string.Empty;
+            }
+
+            byte[] pp = new byte[count];
+            Array.Clear(pp, 0, count);
+            Array.Copy(bytes, index, pp, 0, count);
+            string strAll = BitConverter.ToString(pp);
+            string strHex = strAll.Replace(NativeiOS.m_strSplit, "");
+            int nIndex = strAll.IndexOf(NativeiOS.m_strSplit);
+
+            if (-1 != nIndex)
+            {
+                strHex = strAll.Substring(0, nIndex);
+            }
+            string strNormal = UTF8.GetString(bytes, index, count).Trim('\0'); 
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("_GB2312ToUtf8 BitConverter = " + BitConverter.ToString(pp));
+            sb.Append(" / ");
+            sb.Append("_GB2312ToUtf8 strHex = " + strHex);
+            sb.Append(" / ");
+            sb.Append("_GB2312ToUtf8 strNormal = " + strNormal);
+            Debug.Log(sb.ToString());
+
+            return strHex;
+
+        }
+        //-------------------------------------------------------------------------
+    }
+}
